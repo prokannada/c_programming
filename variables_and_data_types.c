928cbcfcb57f1d08127e0bf6d2ad3aa608c01f79
Programming in Kannada <kannadaprogramming@gmail.com>
2025-02-26 16:37:39 +0530

    Intro to data types and variables in C

---
+++ variables_and_data_types.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int age;
+    float percentage;
+    char gender;
+    bool isEligible;
+
+    age = 20;
+    percentage = 90.67;
+    gender = 'm';
+    isEligible = false;
+
+    printf("value of age is = %d\n", age);
+    printf("value of percentage is = %f\n", percentage);
+    printf("value of gender is = %c\n", gender);
+    printf("value of isEligible is = %d\n", isEligible);
+
+    return 0;
+}
---
+++ variables_and_data_types.md
@@ -0,0 +1,22 @@
+# Intoduction to variables and data types in C Programming.
+
+## Command used for compilation.
+
+```console
+gcc variables_and_data_types.c
+```
+
+## Command used for running the executable.
+
+```console
+./a.out
+```
+
+## Output
+
+```console
+value of age is = 20
+value of percentage is = 90.669998
+value of gender is = m
+value of isEligible is = 0
+```
